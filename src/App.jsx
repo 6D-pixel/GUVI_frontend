0ec6ffbedb281245d630829f149abfcdc176eb81
6D-pixel <dinesh7rd@gmail.com>
2024-02-25 19:25:37 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Signup />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
